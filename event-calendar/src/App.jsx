ed75defb22f002c7d84a72020e02a583dd2db8ea
Dimana Naneva <dimana.naneva@mail.mcgill.ca>
2025-01-18 16:06:27 -0500

    test2

---
+++ event-calendar/src/App.jsx
@@ -0,0 +1,10 @@
+import CalendarApp from "./Components/CalendarApp.css"
+import "./Components/CalendarApp.css"
+
+const app = () => {
+  return (
+    <div className="container"><CalendarApp/></div>
+  )
+}
+
+export default app
--- event-calendar/src/components/CalendarApp.jsx
+++ event-calendar/src/components/CalendarApp.jsx
@@ -1,17 +1,11 @@
-const CalenderApp = () => {
+const CalendarApp = () => {
     return (
-        <div className="calendar-app">
+        <div className="CalendarApp">
             <div className="calendar">
                 <h1 className="heading">Calendar</h1>
-                <div className="navigate-data">
-                    <h2 className="month">May,</h2>
-                    <h2 className="year">2024</h2>
-                    <div className="buttons"></div>
-                </div>
             </div>
         </div>
     )
 }
 
-
-export default CalenderApp
+export default CalendarApp
