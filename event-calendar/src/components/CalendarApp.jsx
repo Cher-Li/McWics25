0f5ec9d24005f4e18c0ce5c304a3afbd33a30864
Maryam Mehdaoui <49500705+MaryMehd@users.noreply.github.com>
2025-01-18 14:41:10 -0500

    component folder

---
+++ event-calendar/src/components/CalendarApp.jsx
@@ -0,0 +1,17 @@
+const CalenderApp = () => {
+    return (
+        <div className="calendar-app">
+            <div className="calendar">
+                <h1 className="heading">Calendar</h1>
+                <div className="navigate-data">
+                    <h2 className="month">May,</h2>
+                    <h2 className="year">2024</h2>
+                    <div className="buttons"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default CalenderApp
