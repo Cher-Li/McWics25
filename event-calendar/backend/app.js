8f4081e21bf8c54d4e5081257f3ab81c5482ac16
Sakura7383 <cherli3c33@gmail.com>
2025-01-18 21:55:13 -0500

    Adding / deleting events

--- event-calendar/backend/app.js
+++ event-calendar/backend/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -10,19 +12,25 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 // Database Connection
 mongoose
-  .connect("mongodb://localhost:27017/scheduleBuilder", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to MongoDB");
   })
-  .catch((err) => console.error(err));
+  .catch((err) => console.log("test"));
 
 // Routes
-app.use("/schedules", scheduleRoutes);
+app.use("/api/schedules", scheduleRoutes);
 
 // Start the Server
 const PORT = process.env.PORT || 5000;
--- event-calendar/src/components/CalendarApp.jsx
+++ event-calendar/src/components/CalendarApp.jsx
@@ -2,18 +2,26 @@ import React, { useState, useEffect } from "react";
 import "./CalendarApp.css";
 import ScheduleList from "./ScheduleList";
 import ScheduleForm from "./ScheduleForm";
+import axios from "axios";
 
 const CalendarApp = () => {
   const [schedules, setSchedules] = useState([]);
 
   useEffect(() => {
-    // Fetch schedules from API
-    const fetchSchedules = async () => {
-      const response = await fetch("/api/schedules");
-      const data = await response.json();
-      setSchedules(data);
-    };
-    fetchSchedules();
+    axios
+      .get("/api/schedules")
+      .then((response) => {
+        console.log("API Response:", response.data);
+        setSchedules(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching schedules:", error);
+        // Log the error details to see what's wrong
+        console.error(
+          "Error details:",
+          error.response ? error.response.data : error.message
+        );
+      });
   }, []);
 
   return (
